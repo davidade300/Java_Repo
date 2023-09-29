d26543b5d74cd86db3b4e419e1fc462f9616fcb7
David Aderaldo <iaderinho@outlook.com>
2023-09-29 14:38:01 -0300

    arquivos inseridos no ScreenMatch

--- Java_OOP/src/ScreenMatch_cs2/Filme.java
+++ Java_OOP/src/ScreenMatch_cs2/Filme.java
@@ -1,7 +1,27 @@
 public class Filme{
-    String nome;
-    int anoDeLancamento;
-    boolean incluidoNoPlano;
-    double avaliacao;
-    int totalDeAvalicoes;
+    String nome; //atributos
+    int anoDeLancamento; //atributos
+    boolean incluidoNoPlano; //atributos
+    double somaDasAvaliacoes; //atributos
+    int totalDeAvalicoes; //atributos
+    int duracaoEmMinutos; // atributos
+    
+    // metodo \/
+    void exibeFichaTecnica(){ //nao tem argumentos portanto () vazio
+        // metodos que nao retornam nenhum argumento nada tem como palavra chave "void"
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lancamento: " + anoDeLancamento);
+    } 
+    
+     // metodo \/
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvalicoes++;
+    }
+
+    // metodo \/
+    double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvalicoes;
+    }
+
 }
---
+++ Java_OOP/src/ScreenMatch_cs2/Principal.java
@@ -0,0 +1,18 @@
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Filme meuFilme = new Filme(); // tipo por referência
+        meuFilme.nome = "Olha eu aqui denovo";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        meuFilme.avalia(6);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvalicoes);
+        System.out.println(meuFilme.pegaMedia());
+    }
+}
