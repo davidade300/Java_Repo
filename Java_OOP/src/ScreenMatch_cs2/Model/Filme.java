8c4412983a0e805f55edfb564978ce1a39b0eab7
David Aderaldo <iaderinho@outlook.com>
2023-10-02 12:59:38 -0300

    refatorados nomes de pacotes

--- Java_OOP/src/ScreenMatch_cs2/Model/Filme.java
+++ Java_OOP/src/ScreenMatch_cs2/Model/Filme.java
@@ -1,62 +1,14 @@
 package Model;
-public class Filme{
-    private String nome; //atributos
-    private int anoDeLancamento; //atributos
-    private boolean incluidoNoPlano; //atributos
-    private double somaDasAvaliacoes; //atributos
-    private int totalDeAvalicoes; //atributos
-    private int duracaoEmMinutos; // atributos
+
+public class Filme extends Titulo{
+    private String diretor; //atributos
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
     
-    
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public boolean isIncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-    // metodo \/
-    public void exibeFichaTecnica(){ //nao tem argumentos portanto () vazio
-        // metodos que nao retornam nenhum argumento nada tem como palavra chave "void"
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lancamento: " + anoDeLancamento);
-    } 
-    
-     // metodo \/
-    public void avalia(double nota){
-        somaDasAvaliacoes += nota;
-        totalDeAvalicoes++;
-    }
-
-    // metodo \/
-    public double pegaMedia(){
-        return somaDasAvaliacoes / totalDeAvalicoes;
-    }
-
 }
---
+++ Java_OOP/src/ScreenMatch_cs2/Model/Titulo.java
@@ -0,0 +1,62 @@
+package Model;
+
+public class Titulo {
+    private String nome; //atributos
+    private int anoDeLancamento; //atributos
+    private boolean incluidoNoPlano; //atributos
+    private double somaDasAvaliacoes; //atributos
+    private int totalDeAvalicoes; //atributos
+    private int duracaoEmMinutos; // atributos
+    private int temporadas;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    // metodo \/
+    public void exibeFichaTecnica(){ //nao tem argumentos portanto () vazio
+        // metodos que nao retornam nenhum argumento nada tem como palavra chave "void"
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lancamento: " + anoDeLancamento);
+    } 
+    
+     // metodo \/
+    public void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvalicoes++;
+    }
+
+    // metodo \/
+    public double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvalicoes;
+    }
+}
