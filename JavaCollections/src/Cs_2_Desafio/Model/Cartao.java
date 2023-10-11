e27d7c4cacbe705589a8deeeec9d496108f85f32
David Aderaldo <iaderinho@outlook.com>
2023-10-11 12:56:42 -0300

    Projeto Migrado do VSC para IntelliJ

--- JavaCollections/src/Cs_2_Desafio/Model/Cartao.java
+++ JavaCollections/src/Cs_2_Desafio/Model/Cartao.java
@@ -1,5 +1,6 @@
 package Cs_2_Desafio.Model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cartao {
@@ -7,8 +8,29 @@ public class Cartao {
     private double saldo;
     private List<Compra> compras;
 
-    public Cartao(double limite, double saldo) {
+    public Cartao(double limite) {
         this.limite = limite;
-        this.saldo = saldo;
+        this.saldo = limite;
+        this.compras = new ArrayList<>();
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compra> getCompras() {
+        return compras;
+    }
+
+    public boolean lancaCompra(Compra compra){
+        if(saldo > compra.getValor()) {
+            this.saldo -= compra.getValor();
+            return true;
+        }
+            return  false;
     }
 }
--- JavaCollections/src/Cs_2_Desafio/Model/Compra.java
+++ JavaCollections/src/Cs_2_Desafio/Model/Compra.java
@@ -1,12 +1,29 @@
 package Cs_2_Desafio.Model;
 
-public class Compra {
+public class Compra implements  Comparable<Compra>{
     private String nomeProduto;
     private double valor;
 
     public Compra(String nomeProduto, double valor) {
-        nomeProduto = nomeProduto;
+        this.nomeProduto = nomeProduto;
         this.valor = valor;
     }
 
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: descricao: " + nomeProduto + " valor: " + valor;
+    }
+
+    @Override
+    public int compareTo(Compra outraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(outraCompra.valor));
+    }
 }
